bdd9dc85623c230c3ac702d7864589b2096f854a
AnaGrazyela <134284369+AnaGrazyela@users.noreply.github.com>
2023-05-22 08:43:54 -0300

    Delete funcao.py

--- funcao.py
+++
@@ -1,9 +0,0 @@
-def verificar_par():
-    numero = int(input("Digite um número: "))
-    if numero % 2 == 0:
-        return "O número é par"
-    else:
-        return "O número é Ímpar"
-    
-resultado = verificar_par()
-print(resultado)
