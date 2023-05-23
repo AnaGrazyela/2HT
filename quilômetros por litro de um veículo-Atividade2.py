dd1dabb2b525d77f5640149e5381efe922910dc0
AnaGrazyela <134284369+AnaGrazyela@users.noreply.github.com>
2023-05-23 10:13:32 -0300

    Create quilômetros por litro de um veículo-Atividade2.py

---
+++ quilômetros por litro de um veículo-Atividade2.py
@@ -0,0 +1,4 @@
+q = float(input("Digite os quilometros rodados:  "))
+l = float(input("Digite a quantidade do litros colocados:  "))
+quilometros_rodados = q/l
+print("Os quilometros rodados do carro é: ", quilometros_rodados)
