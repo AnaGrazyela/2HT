fa2bf3eb7da360512c0cb5d01446c524aa7ceea2
AnaGrazyela <134284369+AnaGrazyela@users.noreply.github.com>
2023-05-23 10:12:27 -0300

    Create volume de água de uma piscina - Atividade2.py

---
+++ volume de água de uma piscina - Atividade2.py
@@ -0,0 +1,5 @@
+l1 = float(input("Insira o valor do lado1:  "))
+l2 = float(input("Insira o valor do lado2:  "))
+profundidade = float(input("Insira o valor da profundidade:  "))
+total = l1*l2*profundidade
+print("O volume é: ", total)
